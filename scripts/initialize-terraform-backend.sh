d2c84862dd02e4cd75d35aa4adbb4e2c32875dd8
Pulin Gupta <pulingupta@Pulins-MBP.home>
2021-03-04 13:02:22 +0200

    remove string uppercourses

--- scripts/initialize-terraform-backend.sh
+++ scripts/initialize-terraform-backend.sh
@@ -6,8 +6,8 @@ export AZURE_STORAGE_ACCOUNT=$storageAccount
 # export TERRAFORM_BREAK_LEASE=1
 export AZURE_STORAGE_KEY="$(az storage account keys list -g "$RG" -n "$AZURE_STORAGE_ACCOUNT" --query '[0].value' -o tsv)"
 if test -z "$AZURE_STORAGE_KEY"; then
-    az configure --defaults group="$RG" location=$location
-    az group create -n "$RG" -o none
+    az configure --defaults group=$RG location=$location
+    az group create -n $RG -o none
     az storage account create -n "$AZURE_STORAGE_ACCOUNT" -o none
     az storage account blob-service-properties update --enable-delete-retention true --delete-retention-days 100 -n "$AZURE_STORAGE_ACCOUNT" -o none
     az storage account blob-service-properties update --enable-versioning -n "$AZURE_STORAGE_ACCOUNT" -o none
