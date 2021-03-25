1d036fb3afe2fdd9bac39af59c200555a8a2a5dd
Pulin Gupta <pulingupta@Pulins-MBP.home>
2021-03-25 12:48:41 +0200

    add remove redirect

--- scripts/clone-repo.sh
+++ scripts/clone-repo.sh
@@ -10,6 +10,11 @@ if [[ "$cloneUrl" == "$SETTINGS_CLONESOURCETERRAFORMCODE" ]]; then
     git clone $([ ! -z $branchName ] && echo -b $branchName) https://$gitToken@$SETTINGS_GITHUBURL/$targetGitOrganization/$targetDir.git
     echo "##[debug] Git clone completed"
 elif [[ "$cloneUrl" == "$SETTINGS_CLONESOURCEWIKI" ]]; then
+    git config --global --list | grep url. | awk -F '.instead' '{print $1}' | while read line
+    do
+    echo $line
+    git config --global --remove-section $line
+    done
     echo "##[debug] Cloning wiki more"
     echo "$GIT_TOKENAZUREDEVOPS"
     if [ -d "$targetDir" ]; then rm -rf $targetDir; fi
