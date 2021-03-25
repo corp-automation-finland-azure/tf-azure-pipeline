f575f2088d81f2fd21c65c7ab0dc75aa158bd901
Pulin Gupta <pulingupta@Pulins-MBP.home>
2021-03-25 13:33:39 +0200

    move redirect stuff under a flag

--- scripts/clone-repo.sh
+++ scripts/clone-repo.sh
@@ -18,7 +18,7 @@ elif [[ "$cloneUrl" == "$SETTINGS_CLONESOURCEWIKI" ]]; then
         done
     if [ -d "$targetDir" ]; then rm -rf $targetDir; fi
     # azdo is generic key for azure devops repos
-    git clone https://azdo:vthvt43skvrd5pqgmfal467ojo54ri4zlrdqw6moiqesvliuwduq@$SETTINGS_AZUREDEVOPSURL/$targetGitOrganization/$targetProject/_git/$targetDir $targetDir
-    echo "##[debug] Git clone completed"
+    git clone https://azdo:elpt5epxodipn77xgjr2clkkqliz3madoh6cqsxjk4uez76bqgdq@$SETTINGS_AZUREDEVOPSURL/$targetGitOrganization/$targetProject/_git/$targetDir $targetDir
+    echo "##[debug] Git clone completed 2"
 fi
 echo "##[endgroup]"
