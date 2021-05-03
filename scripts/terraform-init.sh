54ef6d9227feb5bf800ab6eafc538d3afb159c65
Pulin Gupta <pulingupta@Pulins-MBP.home>
2021-05-03 10:59:38 +0300

    try with cd

--- scripts/terraform-init.sh
+++ scripts/terraform-init.sh
@@ -3,9 +3,9 @@ echo "##[group]Initialise terraform"
 echo "##[debug]  Terraform init script started"
 set -eux  # fail on error
 # terraform -chdir=${terraformWorkingDir}
+cd ${terraformWorkingDir}
 terraform init \
     -upgrade \
-    -backend-config=chdir=${terraformWorkingDir} \
     -backend-config=resource_group_name=${terraformBackendResourceGroup} \
     -backend-config=storage_account_name=${terraformBackendStorageAccount} \
     -backend-config=container_name=${terraformBackendStorageContainer} \
