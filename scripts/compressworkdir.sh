4ff4cda5828673942f5101391a2cd7965885ae1c
Pulin Gupta <pulingupta@Pulins-MBP.home>
2021-05-04 13:23:30 +0300

    push more logs

--- scripts/compressworkdir.sh
+++ scripts/compressworkdir.sh
@@ -2,7 +2,9 @@
 echo "Compressing ${terraformPath} directory..."
 if  [ ! -z ${TERRAFORM_WORKDIR:=""} ]; then
     tar -czf ${terraformRemoteStateFile}.tar.gz -C ${terraformPath}/$TERRAFORM_WORKDIR/ .
+    echo "Compressed $TERRAFORM_WORKDIR directory..."
 else
     tar -czf ${terraformRemoteStateFile}.tar.gz -C ${terraformPath} .
+    echo "Compressed ${terraformPath} directory..."
 fi
 echo "##[debug] Compress terraform directory done"
