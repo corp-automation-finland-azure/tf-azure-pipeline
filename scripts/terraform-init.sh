382302087e6b17313130a77d313954900f4eb63a
Pulin Gupta <pulingupta@Pulins-MBP.home>
2021-05-04 10:16:09 +0300

    try with -z option

--- scripts/terraform-init.sh
+++ scripts/terraform-init.sh
@@ -2,7 +2,7 @@
 echo "##[group]Initialise terraform"
 echo "##[debug]  Terraform init script started"
 set -eux  # fail on error
-if  [ ! -z ${terraformWorkingDir} ]; then
+if  [ ! -z $TERRAFORM_WORKDIR ]; then
  cd ${terraformWorkingDir}
 fi
 
